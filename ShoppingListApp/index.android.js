1e52e0ca0e035d7909274e8748ab11027dc6cca1
Adrian Pupaza <pupazadrian@gmail.com>
2017-01-10 21:38:32 +0200

    Work in progress

--- ShoppingListApp/index.android.js
+++ ShoppingListApp/index.android.js
@@ -6,48 +6,101 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    Text,
+    View,
+    Navigator,
+    StyleSheet
 } from 'react-native';
+import MyScene from './MyScene';
+import WelcomeScene from './WelcomeScene'
 
 export default class ShoppingListApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        function renderScene(route, navigator) {
+            if (route.passProps.sceneType === "welcome") {
+                return <WelcomeScene styles={styles} navigator={navigator} {...route.passProps}/>
+            }
+            else {
+                <BasicComponent hello="Hello world!"/>
+            }
+        }
+        return (
+            //     <MyScene
+            //         title={route.passProps.title}
+            //         sceneType={route.passProps.sceneType}
+            //         // Function to call when a new scene should be displayed
+            //         onForward={(sceneTypeProp, titleProp) => {
+            //           const nextIndex = route.index + 1;
+            //           navigator.push({
+            //             index: nextIndex,
+            //             passProps:{
+            //                 sceneType: sceneTypeProp,
+            //                 title: titleProp,
+            //             },
+            //           });
+            //         }}
+            //         // Function to call to go back to the previous scene
+            //         onBack={() => {
+            //           if (route.index > 0) {
+            //             navigator.pop();
+            //           }
+            //         }}
+            //         }
+            // />
+            <Navigator
+                style={{flex:1}}
+                initialRoute={{ passProps: { sceneType:"welcome" , index:0}}}
+                renderScene={renderScene}
+            />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    fullWidthButton: {
+        backgroundColor: 'deepskyblue',
+        height:40,
+        width:100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: 'azure'
+    }
 });
+
+class BasicComponent extends Component {
+    render() {
+        function sayHello(){
+            return <View>
+                <Text>{this.props.hello}</Text>
+                <Text>Hello</Text>
+                </View>
+        }
+        return (
+        sayHello()
+        );
+    }
+}
 
 AppRegistry.registerComponent('ShoppingListApp', () => ShoppingListApp);
